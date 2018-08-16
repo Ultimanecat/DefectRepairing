0c5a14693609dc8b3048b1d954164807f8c9a769
liuxinyuan <lxy_pku@qq.com>
2018-08-16 10:41:32 +0800

    add pattern of TargetMethod Btrace script

---
+++ tool/source/lib/btrace/TargetMethod_pattern.java
@@ -0,0 +1,16 @@
+import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+
+@BTrace
+public class TargetMethod {
+    @OnMethod(
+        clazz="__CLASS__NAME__",
+        method="__METHOD__NAME__",
+        type="__SIGNATURE__"
+        location=@Location(value=Kind.LINE, line=-1)
+    )
+    public static void online(@ProbeClassName String pcn, @ProbeMethodName String pmn, int line) {
+        print("---" + pcn + "." + pmn +  ":" + line + "\n");
+    }
+    
+}
